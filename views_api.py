71fc23f20a7b902f85888fd75ee3a17999d8cf3f
Abhiram <mabhirama06@hotmail.com>
2023-03-06 16:11:11 +0530

    1

--- views_api.py
+++ views_api.py
@@ -36,7 +36,7 @@ async def api_atmbitbits(
 
 @atmbitbit_ext.get("/api/v1/fetch_atm/{api_key_id}")
 async def api_atmbitbits(
-    api_key_id=Depends(require_admin_key)
+      api_key_id, wallet: WalletTypeInfo = Depends(require_admin_key)
 ):
 
     return (await get_atmbitbit_by_api_key_id(api_key_id)).dict()
